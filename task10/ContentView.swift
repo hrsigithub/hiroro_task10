daaa8312f8f3fde28733c686f04c30500ed8d03e
ひろろ <hrsi.ad@gmail.com>
2021-02-21 10:47:35 +0900

    Initial Commit

---
+++ task10/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  task10
+//
+//  Created by Hiroshi.Nakai on 2021/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ task10/task10App.swift
@@ -0,0 +1,17 @@
+//
+//  task10App.swift
+//  task10
+//
+//  Created by Hiroshi.Nakai on 2021/02/21.
+//
+
+import SwiftUI
+
+@main
+struct task10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
